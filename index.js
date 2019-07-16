a1eb336b4662b3a931d955bcdc9b499e413890dc
raberin <rmvaberin@gmail.com>
2019-07-15 18:23:29 -0700

    Merging fixes

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
+=======
+const server = require('./api/server');
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\nAPI running on port ${port}\n`));
+>>>>>>> f40747e74fdcac5f99c8d089f7e45e7b29a0f668
--- knexfile.js
+++ knexfile.js
@@ -5,44 +5,44 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './api/db/dev-party.sqlite3'
+      filename: './api/db/devparty.sqlite3'
     },
     migrations: {
       directory: './api/db/migrations'
     },
     useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
+  //
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+  //
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
 
 };
