83e432df35a37853b985550c20002505b49ba54d
raberin <rmvaberin@gmail.com>
2019-07-15 19:45:08 -0700

    Fully working party route

--- api/data/routes/party/party.js
+++ api/data/routes/party/party.js
@@ -76,10 +76,14 @@ router.put("/:id", checkToken, async (req, res) => {
   const { id } = req.params;
   try {
     const updatingParty = await partyModel.updateParty(id, req.body);
-    res.status(200).json(updatingParty);
+    if (updatingParty) {
+      res.status(200).json(updatingParty);
+    } else {
+      res.status(404).json({ message: "Error in updating party" });
+    }
   } catch (err) {
     res.status(500).json({
-      message: "Error updating party"
+      message: "Error updating party I'm at the catch"
     });
   }
 });
