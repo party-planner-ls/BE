9d6660308e67a5ff670e2c801c596a14b3fbaf6a
raberin <rmvaberin@gmail.com>
2019-07-15 21:32:36 -0700

    Working shopping_list endpoint for the most part - still needs tweaking

--- api/data/routes/shopping_list/shoppingList.js
+++ api/data/routes/shopping_list/shoppingList.js
@@ -18,7 +18,7 @@ const router = express.Router();
 //Endpoints
 router.get("/", checkToken, async (req, res) => {
   try {
-    const shoppingList = await shoppingListModel.getShoppingList();
+    const shoppingList = await shoppingListModel.getShoppingListWithItems();
     res.status(200).json(shoppingList);
   } catch (err) {
     res
@@ -30,7 +30,9 @@ router.get("/", checkToken, async (req, res) => {
 router.get("/:id", checkToken, async (req, res) => {
   const { id } = req.params;
   try {
-    const shoppingList = await shoppingListModel.getShoppingListById(id);
+    const shoppingList = await shoppingListModel.getShoppingListByIdWithItems(
+      id
+    );
     if (shoppingList) {
       res.status(200).json(shoppingList);
     } else {
@@ -40,6 +42,56 @@ router.get("/:id", checkToken, async (req, res) => {
     res
       .status(500)
       .json({ message: "We ran into an error retrieving the shopping list" });
+  }
+});
+
+//Adding the party_id is basically the req.body
+router.post("/", checkToken, async (req, res) => {
+  //party_id === req.body to attach to right party
+  const shoppingList = req.body;
+  try {
+    const addShoppingList = await shoppingListModel.addShoppingList(
+      shoppingList
+    );
+    res.status(200).json(addShoppingList);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error adding shopping list"
+    });
+  }
+});
+
+router.delete("/:id", checkToken, async (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).json({ message: "missing ID or wrong ID" });
+  } else {
+    try {
+      const deletedShoppingList = await shoppingListModel.deleteShoppingList(
+        id
+      );
+      res.status(204).json(deletedShoppingList);
+    } catch (err) {
+      res.status(500).json({
+        message: "Error deleting shopping list"
+      });
+    }
+  }
+});
+
+router.put("/:id", checkToken, async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatingParty = await partyModel.updateParty(id, req.body);
+    if (updatingParty) {
+      res.status(200).json(updatingParty);
+    } else {
+      res.status(404).json({ message: "Error in updating party" });
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Error updating party I'm at the catch"
+    });
   }
 });
 
